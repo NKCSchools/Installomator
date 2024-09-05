3e7a0c8d8031046a8cbc0325d44d5ce9be150e45
brentac <31676385+brentac@users.noreply.github.com>
2024-09-05 07:44:40 -0500

    Create edibleinkprinter24n.sh

---
+++ fragments/nkc/edibleinkprinter24n.sh
@@ -0,0 +1,8 @@
+edibleinkprinter24n)
+    name="EdibleInkPrinter-2.4-N"
+    type="pkgInDmg"
+    packageID="com.primera.Eddie"
+    downloadURL="https://www.primera.com/support/exe/Eddie/EdibleInkPrinter-2.4-N.dmg"
+    appNewVersion=""
+    expectedTeamID="56Y8447L2Y"
+    ;;
