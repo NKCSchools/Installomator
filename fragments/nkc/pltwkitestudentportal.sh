6671872db778a5c77897572564e35b291c1a6c7f
brentac <31676385+brentac@users.noreply.github.com>
2025-03-31 08:35:14 -0500

    Update pltwkitestudentportal.sh

--- fragments/nkc/pltwkitestudentportal.sh
+++ fragments/nkc/pltwkitestudentportal.sh
@@ -2,7 +2,7 @@ pltwkitestudentportal)
     # NKC Change
     name="PLTW Kite Student Portal"
     type="dmg"
-    downloadURL="https://files.kiteaai.org/installers/pltwstudentportal/2023/PLTW%20Kite%20Student%20Portal.dmg"
+    downloadURL="https://files.kiteaai.org/installers/pltwstudentportal/prod/latest/mac/PLTW%20Kite%20Student%20Portal.dmg"
     appNewVersion=""
     expectedTeamID="BK4732M7XX"
     ;;
