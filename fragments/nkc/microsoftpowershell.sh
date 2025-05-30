f50c47df5d8cdf76e72344a22b9fcf63385271b3
brentac <31676385+brentac@users.noreply.github.com>
2025-05-30 14:46:50 -0500

    Delete microsoftpowershell.sh

--- fragments/nkc/microsoftpowershell.sh
+++
@@ -1,9 +0,0 @@
-microsoftpowershell)
-    # NKC Change
-    name="Microsoft PowerShell"
-    type="pkg"
-    downloadURL=$(downloadURLFromGit PowerShell PowerShell )
-    appNewVersion=$(versionFromGit PowerShell PowerShell )
-    expectedTeamID="UBF8T346G9"
-    ;;
-    
