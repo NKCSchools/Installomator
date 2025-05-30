97164d65e78ea541beced383a01613bdaf5a75c6
brentac <31676385+brentac@users.noreply.github.com>
2025-05-30 09:26:47 -0500

    Create razersynapseinstaller.sh

---
+++ fragments/nkc/razersynapseinstaller.sh
@@ -0,0 +1,12 @@
+razersynapseinstaller)
+    name="RazerSynapseInstaller"
+    type="pkg"
+    packageID="com.razer.install.AppInstall
+com.razer.Synapse.entry.install
+com.razer.install.EngineInstall
+com.razer.Synapse.engine.install"
+    downloadURL="https://rzr.to/synapse-4-mac-download"
+    appNewVersion=""
+    expectedTeamID="R2H967U7J8"
+    ;;
+    
