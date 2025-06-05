16117ce3136f99e463249cc3ae184cf95f08e4d2
brentac <31676385+brentac@users.noreply.github.com>
2025-06-05 11:01:30 -0500

    update installomator

--- custom/Installomator.sh
+++ custom/Installomator.sh
@@ -1594,14 +1594,14 @@ cricutdesignspace)
     downloadURL=$(getJSONValue $(curl  -fsL "https://apis.cricut.com/desktopdownload/InstallerFile?shard=a&operatingSystem=osxnative&fileName=CricutDesignSpace-Install-v${appNewVersion}.dmg") "result")
     expectedTeamID="25627ZFVT7"
     ;;
-displaylinkmanagernkc)
+displaylinkmanager)
     name="DisplayLink Manager"
     type="pkgInZip"
     appNewVersion=$(curl -sfL https://www.synaptics.com/products/displaylink-graphics/downloads/macos | grep -m 1 "Release:" | cut -d ' ' -f2)
     downloadURL="https://www.synaptics.com$(version_no_dots="${appNewVersion//./}" && intermediate_page_url="https://www.synaptics.com/products/displaylink-manager-graphics-connectivity-${version_no_dots}?filetype=exe" && curl -sfL "${intermediate_page_url}" | grep -o -E 'href="[^"]*DisplayLink%20Manager%20Graphics%20Connectivity[^"]*EXE\.zip[^"]*"' | head -n 1 | sed 's/href="//' | sed 's/"//')"
     expectedTeamID="73YQY62QM3"
     ;;
-easeldriver)
+    easeldriver)
     name="EaselDriver"
     type="pkg"
     packageID="com.inventables.iris-node
--- fragments/nkc/displaylinknkc.sh
+++ fragments/nkc/displaylinknkc.sh
@@ -1,7 +1,8 @@
-displaylinkmanagernkc)
+displaylinkmanager)
     name="DisplayLink Manager"
     type="pkgInZip"
     appNewVersion=$(curl -sfL https://www.synaptics.com/products/displaylink-graphics/downloads/macos | grep -m 1 "Release:" | cut -d ' ' -f2)
     downloadURL="https://www.synaptics.com$(version_no_dots="${appNewVersion//./}" && intermediate_page_url="https://www.synaptics.com/products/displaylink-manager-graphics-connectivity-${version_no_dots}?filetype=exe" && curl -sfL "${intermediate_page_url}" | grep -o -E 'href="[^"]*DisplayLink%20Manager%20Graphics%20Connectivity[^"]*EXE\.zip[^"]*"' | head -n 1 | sed 's/href="//' | sed 's/"//')"
     expectedTeamID="73YQY62QM3"
     ;;
+    
