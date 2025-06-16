6ebcaeb1c16a8663138e0010ee8490d6cfb6ab4e
brentac <31676385+brentac@users.noreply.github.com>
2025-06-16 08:29:50 -0500

    Splunk

---
+++ fragments/nkc/splunkforwarder.sh
@@ -0,0 +1,8 @@
+splunkforwarder)
+    name="SplunkForwarder"
+    type="pkgInDmg"
+    downloadURL="https://download.splunk.com/products/universalforwarder/releases/9.4.2/macOS/SplunkUniversalForwarder-9.4.2-a7f645ddaf91-darwin-universal.dmg"
+    appNewVersion="9.4.2"
+    pkgName=".payload/SplunkForwarder_9.4.2.pkg"
+    expectedTeamID="4HBT8Q2X5G"
+    ;;
