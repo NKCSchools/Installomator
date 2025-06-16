13be3db250e03cced9c7f01be6028e0f1799b30a
brentac <31676385+brentac@users.noreply.github.com>
2025-06-16 08:41:38 -0500

    splunk work

--- custom/Installomator.sh
+++ custom/Installomator.sh
@@ -1743,12 +1743,23 @@ simpleinout)
     ;;
 splunkforwarder)
     name="SplunkForwarder"
-    type="pkgInDmg"
+    type="dmg"
     downloadURL="https://download.splunk.com/products/universalforwarder/releases/9.4.2/macOS/SplunkUniversalForwarder-9.4.2-a7f645ddaf91-darwin-universal.dmg"
     appNewVersion="9.4.2"
-    pkgName=".payload/SplunkForwarder_9.4.2.pkg"
     expectedTeamID="4HBT8Q2X5G"
-    ;;testnav)
+    installerTool="custom"
+    installCommand='
+        pkgPath=$(find "$tmpDir" -type f -path "*/.payload/*.pkg" -name "*.pkg" | head -n 1)
+        if [ -f "$pkgPath" ]; then
+            echo "Installing SplunkForwarder from $pkgPath"
+            /usr/sbin/installer -pkg "$pkgPath" -target /
+        else
+            echo "ERROR: Could not find SplunkForwarder pkg inside mounted dmg"
+            exit 1
+        fi
+    '
+    ;;
+    testnav)
     name="installer"
     type="dmg"
     dmgname=$(curl -s https://download.testnav.com/installerVersions.json | grep mac | cut -d ":" -f 2 | cut -d '"' -f 2)
--- fragments/nkc/splunkforwarder.sh
+++ fragments/nkc/splunkforwarder.sh
@@ -1,8 +1,19 @@
 splunkforwarder)
     name="SplunkForwarder"
-    type="pkgInDmg"
+    type="dmg"
     downloadURL="https://download.splunk.com/products/universalforwarder/releases/9.4.2/macOS/SplunkUniversalForwarder-9.4.2-a7f645ddaf91-darwin-universal.dmg"
     appNewVersion="9.4.2"
-    pkgName=".payload/SplunkForwarder_9.4.2.pkg"
     expectedTeamID="4HBT8Q2X5G"
+    installerTool="custom"
+    installCommand='
+        pkgPath=$(find "$tmpDir" -type f -path "*/.payload/*.pkg" -name "*.pkg" | head -n 1)
+        if [ -f "$pkgPath" ]; then
+            echo "Installing SplunkForwarder from $pkgPath"
+            /usr/sbin/installer -pkg "$pkgPath" -target /
+        else
+            echo "ERROR: Could not find SplunkForwarder pkg inside mounted dmg"
+            exit 1
+        fi
+    '
     ;;
+    
