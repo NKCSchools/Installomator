d4cbe934533a2c12df6deb86919bb791d2e23b41
brentac <31676385+brentac@users.noreply.github.com>
2025-06-17 14:17:59 -0500

    logi options plus offline

---
+++ fragments/nkc/logioptionsplusinstalleroffline.sh
@@ -0,0 +1,8 @@
+logioptionsplusinstalleroffline)
+	# NKC Change
+    name="logioptionsplus_installer_offline"
+    type="zip"
+    downloadURL="https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer_offline.zip"
+    appNewVersion=""
+    expectedTeamID="QED4VVPZWA"
+    ;;
