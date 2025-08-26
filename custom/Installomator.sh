d95473a6ca48f66851f52fd2f969b68d1456bfa9
brentac <31676385+brentac@users.noreply.github.com>
2025-08-26 09:37:54 -0500

    chmodbpf

--- custom/Installomator.sh
+++ custom/Installomator.sh
@@ -1585,7 +1585,7 @@ adobecreativecloudnkc)
     appName="Arduino IDE.app"
     ;;
 chmodbpf)
-    name="Wireshark"
+    name="ChmodPBF"
     type="pkgindmg"
     if [[ $(arch) == i386 ]]; then
       sparkleFeedURL="https://www.wireshark.org/update/0/Wireshark/4.0.0/macOS/x86-64/en-US/stable.xml"
