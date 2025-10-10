2648debd9400c925913ff59798ce230e571c1add
brentac <31676385+brentac@users.noreply.github.com>
2025-10-10 09:28:05 -0500

    Update Edible Ink

--- fragments/nkc/edibleinkprinter24n.sh
+++ fragments/nkc/edibleinkprinter24n.sh
@@ -2,7 +2,7 @@ edibleinkprinter24n)
     name="EdibleInkPrinter-2.4-N"
     type="pkgInDmg"
     packageID="com.primera.Eddie"
-    downloadURL="https://www.primera.com/support/exe/Eddie/EdibleInkPrinter-2.4-N.dmg"
+    downloadURL="https://www.primera.com/support/exe/Eddie/Eddie_2.8_CookiePro_1.0.dmg"
     appNewVersion=""
     expectedTeamID="56Y8447L2Y"
     ;;
