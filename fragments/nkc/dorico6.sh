d5c895eb93b3f441c6b8cbdaf8090600498ef110
brentac <31676385+brentac@users.noreply.github.com>
2025-11-06 15:30:52 -0600

    Dorico 6.1.1

---
+++ fragments/nkc/dorico6.sh
@@ -0,0 +1,8 @@
+dorico6)
+    name="Dorico_6.1.10_Installer_mac"
+    type="pkgInDmg"
+    packageID="com.steinberg.dorico6"
+    downloadURL="https://download.steinberg.net/support/temporary/Dorico_6.1.10/Dorico_6.1.10_Installer_mac.dmg"
+    appNewVersion=""
+    expectedTeamID="5PMY476BJ6"
+    ;;
--- utils/buildLabel.sh
+++ utils/buildLabel.sh
@@ -5,7 +5,7 @@ export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 # downloadURL=${1?:"need to provide a download URL."}
 # sample url for github downloadURL="https://github.com/jamf/Replicator/releases/latest/download/Replicator.zip
 # Jamf team ID PS2F6S478M
-downloadURL="https://steinberg.net/slm-mac"
+downloadURL="https://download.steinberg.net/support/temporary/Dorico_6.1.10/Dorico_6.1.10_Installer_mac.dmg"
 
 # Note: this tool _very_ experimental and does not work in many cases
 # That being said, it's a great place to start for building up the label in the Case-statement
