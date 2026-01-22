5d17e0fae20554f433a994577f5ff6b9636b12ea
brentac <31676385+brentac@users.noreply.github.com>
2026-01-22 09:24:10 -0600

    auto reload

---
+++ fragments/nkc/autoreload.sh
@@ -0,0 +1,7 @@
+autoreload)
+    name="Auto Reload"
+    type="zip"
+    downloadURL="$(downloadURLFromGit garrettrayj auto-reload)"
+    appNewVersion="$(versionFromGit garrettrayj auto-reload)"
+    expectedTeamID="N7TC759VWV"
+    ;;
--- utils/buildLabel.sh
+++ utils/buildLabel.sh
@@ -5,7 +5,7 @@ export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 # downloadURL=${1?:"need to provide a download URL."}
 # sample url for github downloadURL="https://github.com/jamf/Replicator/releases/latest/download/Replicator.zip
 # Jamf team ID PS2F6S478M
-downloadURL="https://download.steinberg.net/support/temporary/Dorico_6.1.10/Dorico_6.1.10_Installer_mac.dmg"
+downloadURL="https://github.com/garrettrayj/auto-reload/releases/download/2.1.0/Auto.Reload.app.zip"
 
 # Note: this tool _very_ experimental and does not work in many cases
 # That being said, it's a great place to start for building up the label in the Case-statement
