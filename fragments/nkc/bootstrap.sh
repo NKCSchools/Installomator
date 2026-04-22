2b0dca66d15d06df7ed137581ff48cadee171981
brentac <31676385+brentac@users.noreply.github.com>
2026-04-22 12:02:58 -0500

    Create bootstrap.sh

---
+++ fragments/nkc/bootstrap.sh
@@ -0,0 +1,8 @@
+bootstrapbuddy)
+    name="BootstrapBuddy"
+    type="pkg"
+    packageID="com.inetum.Bootstrap-Buddy"
+    downloadURL="$(downloadURLFromGit Inetum-Poland bootstrap-buddy)"
+    appNewVersion="$(versionFromGit Inetum-Poland bootstrap-buddy)"
+    expectedTeamID="C8GR948K5Q"
+    ;;
