fc750453e4793081277faaf7956754aa01b9ae24
brentac <31676385+brentac@users.noreply.github.com>
2026-05-21 11:16:30 -0500

    kite

--- custom/Installomator.sh
+++ custom/Installomator.sh
@@ -1716,9 +1716,8 @@ javajdk)
     # NKC Change
     # Download link found at https://ksassessments.org/node/147
     name="Kite Student Portal"
-    type="pkgInDmg"
-    downloadURL="https://files.kiteaai.org/installers/pltwstudentportal/prod/latest/mac/PLTW%20Kite%20Student%20Portal.dmg"
-    # downloadURL="https://files.kiteaai.org/installers/studentportal/prod/latest/mac/Kite%20Student%20Portal.pkg"
+    type="pkg"
+    downloadURL="https://files.kiteaai.org/installers/studentportal/prod/latest/mac/Kite%20Student%20Portal.pkg"
     appNewVersion=""
     expectedTeamID="BK4732M7XX"
     ;;locklizard)
