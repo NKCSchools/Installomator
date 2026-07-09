b3408c695efa9d82cbc55f4b5fa5bab0a4f3f5b4
brentac <31676385+brentac@users.noreply.github.com>
2026-07-09 14:40:31 -0400

    Update nkc-labels branch

--- custom/Installomator.sh
+++ custom/Installomator.sh
@@ -1878,16 +1878,6 @@ com.intuitibits.wifiexplorerpro-helper.pkg"
     appNewVersion=""
     expectedTeamID="2B9R362QNU"
     ;;
-wifiexplorerpro3)
-    # NKC Change
-    name="WiFiExplorerPro3"
-    type="pkg"
-    packageID="com.intuitibits.wifiexplorerpro3.pkg
-com.intuitibits.wifiexplorerpro-helper.pkg"
-    downloadURL="https://www.intuitibits.com/download/wifiexplorerpro3"
-    appNewVersion=""
-    expectedTeamID="2B9R362QNU"
-    ;;
 1password7)
     name="1Password 7"
     type="pkg"
