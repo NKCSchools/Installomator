86ba755bd9fa9bf8009bf559d0f53eb7b17063c5
brentac <31676385+brentac@users.noreply.github.com>
2026-07-09 14:38:57 -0400

    Update nkc-labels branch

---
+++ fragments/nkc/wifiexplorer3 copy.sh
@@ -0,0 +1,10 @@
+wifiexplorerpro3)
+    # NKC Change
+    name="WiFiExplorerPro3"
+    type="pkg"
+    packageID="com.intuitibits.wifiexplorerpro3.pkg
+com.intuitibits.wifiexplorerpro-helper.pkg"
+    downloadURL="https://www.intuitibits.com/download/wifiexplorerpro3"
+    appNewVersion=""
+    expectedTeamID="2B9R362QNU"
+    ;;
